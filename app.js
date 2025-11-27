ea8b411cfa250c9bbdcce659228677b20f58ccc6
NSiddeley <nsiddeley@gmail.com>
2025-11-27 18:38:05 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const port = 5000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World, from Docker!');
+});
+
+server.listen(port, () => console.log(`Server running on http://localhost:${port}`));
